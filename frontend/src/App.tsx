b07b6050b51714f412bce855548787ff1ba3b5a6
Nurlan Rahimli <nurlanrahimli@gmail.com>
2026-08-18 10:25:32 -0700

    feat: implement protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,21 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 
+import ProtectedRoute from "./auth/ProtectedRoute"
 import LoginPage from "./pages/auth/LoginPage"
 import RegisterPage from "./pages/auth/RegisterPage"
+import DashboardPage from "./pages/dashboard/DashboardPage"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/register" replace />} />
+
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
+
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Route>
     </Routes>
   )
 }
